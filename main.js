921aabf734e543d0010ca57ca83745e445ffa5d9
Fedaa <fadooshjad2001@gmail.com>
2020-10-09 14:56:25 +0300

    assissmant-week-1

--- main.js
+++ main.js
@@ -7,6 +7,8 @@
 
 function percentOf(num1, num2) {
   // TODO: your code here
+  //just we have to return the result
+  return num1 + " " + "is" + " " + num1 * num2 + "%" + " " + "of" + " " + num2;
 }
 
 //=============================================== 2
@@ -20,6 +22,14 @@ function percentOf(num1, num2) {
 
 function pluralize(noun, number) {
   // TODO: your code here
+  //we have if statment for the number if it 1 or no
+  if(number === 1){
+  	// if the number = 1 
+  	return (number + " " + noun)
+  }else{
+  	//if the number != 1
+  	return (number + " " + noun + "s")
+  }
 }
 
 //=============================================== 3
@@ -31,6 +41,8 @@ function pluralize(noun, number) {
 
 function average(num1, num2, num3) {
   // TODO: your code here
+  //here just we have to write the operatore and return the result
+  return(num1+num2+num3)/3
 }
 
 //=============================================== 4
@@ -42,6 +54,15 @@ function average(num1, num2, num3) {
 
 function wasItFunny(response) {
   // TODO: your code here
+  //we write if statment if the response true 
+  if(response===true){
+  	//it should return "HAHAHA"
+  	return "HAHAHA";
+  	//if the response = false
+  }if (response===false){
+  	//it should return "meh" 
+  	return "meh";
+  }
 }
 
 //=============================================== 5
@@ -54,6 +75,8 @@ function wasItFunny(response) {
 
 function get6thChar(str) {
   // TODO: your code here
+  //here we use chareAt(index the number) and return it
+  return str.charAt(10);
 }
 //=============================================== 6
 
@@ -66,6 +89,17 @@ function get6thChar(str) {
 
 function oddSum(n) {
   // TODO: your code here
+  var sum = 0;
+  //here we put for loop in the numbers before the n
+  for(var i =0; i < n; i++){
+  	//make if statment to jast take the odd numbers
+  	if(i%2===1){
+  		sum = sum + i;
+  	}
+
+  }
+  //return sum(result)
+  return sum;
 }
 
 //=============================================== 7
@@ -83,6 +117,11 @@ Write a function that uses console.log to give the following triangle:
 
 function drawTriangle() {
   // TODO: your code here
+  var x= " ";
+  for(var i =1; i<= 7; i++){
+  	x = x + "#"
+  	console.log(x)
+  }
 }
 
 //=============================================== 8
@@ -90,11 +129,15 @@ function drawTriangle() {
 //In your own words, no more than 3 sentences, answer the following:
 //What is Function parameter?
 //Write your answer here...
+parameter variables are so important we use into functions
 //What is function argument?
 //Write your answer here...
+ is a value that must be provided to obtain the functions result
 //What is function signature?
 //Write your answer here...
+input and output of functions or methods.
 //What is a variable?
 //Write your answer here...
+something that changes or that can be changed 
 
 //Good Luck :))
